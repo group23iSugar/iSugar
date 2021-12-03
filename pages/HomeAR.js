9e5fa8596f3611a41f19ae50390991544a5d7648
Dina-net2 <94227506+Dina-net2@users.noreply.github.com>
2021-12-03 20:34:16 +0300

    Create HomeAR.js

---
+++ pages/HomeAR.js
@@ -0,0 +1,188 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Image,
+  Button,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+  Dimensions,
+  AppRegistry,
+  Navigator,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  Switch,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const HomeAR = () => {
+  return (
+    <LinearGradient colors={['#AABED8', '#fff']} style={styles.container}>
+      <View style={{top: 10, alignItems: 'center'}}>
+        <Image source={require('../images/logo.png')} style={styles.pic} />
+      </View>
+      <ScrollView style={styles.contView}>
+        <Text
+          style={{
+            color: '#000',
+            fontSize: 25,
+            textAlign: 'center',
+            paddingTop: 20,
+            paddingLeft: 15,
+            fontWeight: 'bold',
+          }}>
+          Home
+        </Text>
+
+        <Text style={styles.inpTxt}>Glucose level in the last 7 days:</Text>
+
+        { <Image
+          source={require('../images/chart.png')}
+          style={{
+            margin: 10,
+            height: 200,
+            width: 270,
+            alignSelf: 'center',
+            borderColor: 'black',
+            borderWidth: 1,
+          }}
+        /> }
+
+        <Text
+          style={{
+            fontWeight: 'bold',
+            textAlign: 'center',
+            margin: 30,
+            borderColor: 'gray',
+            borderWidth: 1,
+          }}>
+          + إضافة قراءة جديدة
+        </Text>
+
+        <Text style={{textAlign: 'center', fontSize: 20, borderWidth: 1 , borderColor: 'gray'}}>
+          Percentage of time in last 7-days in target range: 95%
+        </Text>
+        <Text style={styles.inpTxt}>:المواعيد القادمة</Text>
+        <Text style={{textAlign: 'center', fontSize: 20, borderWidth: 1 , borderColor: 'gray', paddingTop: 40}}>
+        
+        </Text>
+      </ScrollView>
+    </LinearGradient>
+  );
+};
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.15;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  prefix: {
+    backgroundColor: '#9c4',
+  },
+  text: {
+    color: '#000',
+    fontSize: 30,
+  },
+  pic: {
+
+      width: height_logo,
+      height: height_logo,
+ 
+  },
+  inputT: {
+    //inputs field
+
+    width: 110,
+    fontSize: 16,
+    shadowColor: '#000',
+    height: 50,
+    textAlign: 'center',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 0.62,
+
+    elevation: 2,
+  },
+
+  contView: {
+    //Conten's view
+    backgroundColor: '#fff',
+    height: 550,
+    width: 360,
+    alignSelf: 'center',
+    top: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 7,
+  },
+
+  inpTxt: {
+    //lables
+    paddingLeft: 20,
+    paddingTop: 15,
+    fontSize: 18,
+  },
+
+  vNext: {
+    // to make items next to each other
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingTop: 30,
+  },
+
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    padding: 10,
+    width: 300,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    paddingLeft: 30,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 0.9,
+
+    elevation: 3,
+  },
+
+  ddown: {
+    //drop down list style
+
+    paddingLeft: 0,
+    paddingTop: 13,
+    shadowColor: '#000',
+
+    height: 40,
+    width: 160,
+
+    alignItems: 'center',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 0.62,
+
+    elevation: 2,
+    backgroundColor: '#f5f5f5',
+  },
+});
+
+export default HomeAR;
