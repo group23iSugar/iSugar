95b6c91ef1429c8cf293c0cc13bfd7678a663e9a
raneemfaisal <77919227+raneemfaisal@users.noreply.github.com>
2021-12-06 05:04:42 +0300

    Add files via upload

---
+++ pages/CustomDrawerAR.js
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+
+import React from 'react';
+import {ImageBackground, View, Text} from 'react-native';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const CustomDrawer = props => {
+  return (
+    <View style={{flex: 1}}>
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{backgroundColor: '#e9ebee'}}>
+          <ImageBackground source= {require('../images/lightGray.jpg')} style={{padding: 20}}>
+            <View style={{flexDirection: 'row', alignContent: 'center'}}>
+            <MaterialCommunityIcons name="balloon" color="#fff" size={22}/>
+             <Text style={{color: '#fff', fontSize: 18, fontFamily: 'Roboto-Medium', fontWeight: 'bold'}}>مرحبًا بك </Text>
+            </View>
+          </ImageBackground>
+        <View style={{flex: 1, backgroundColor: '#fff', paddingTop: 20}}>
+          <DrawerItemList {...props} />
+        </View>
+      </DrawerContentScrollView>
+    </View>
+  );
+};
+
+export default CustomDrawer;
