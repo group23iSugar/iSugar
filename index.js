aecc2d953aee878eee4d104bd68fad9894a015c7
raneemfaisal <77919227+raneemfaisal@users.noreply.github.com>
2022-02-06 22:11:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,23 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './stackNav';
 import {name as appName} from './app.json';
+import PushNotification from 'react-native-push-notification';
+import { Platform } from 'react-native';
 
 var React = require('react-native');
-var SQLite = require('react-native-sqlite-storage')
+var SQLite = require('react-native-sqlite-storage');
 
+PushNotification.configure({
+    onNotification: function (notification) {
+      console.log('NOTIFICATION:', notification);
+    },
+    requestPermissions: Platform.OS === 'ios',
+  });
+
+  PushNotification.createChannel(
+    {
+      channelId: 'channel-id', // (required)
+      channelName: 'My channel', // (required)
+    },
+  );
 AppRegistry.registerComponent(appName, () => App);
