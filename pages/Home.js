afc4e0cd59990598a8ff2e0eca1f06771db67054
Dina-net2 <94227506+Dina-net2@users.noreply.github.com>
2022-03-14 00:36:03 +0300

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -13,6 +13,7 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   Switch,
+  FlatList,
   Dimensions,
 } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
@@ -28,7 +29,9 @@ import moment from 'moment';
 const Home = ({navigation}) => {
   useEffect(() => {
     dashBoard();
-  },[]);
+    ret3();
+    //ret4();
+  }, []);
 
   const [flag, setFlag] = useState(false);
   const [within, setWitin] = useState(0);
@@ -37,45 +40,73 @@ const Home = ({navigation}) => {
   const [bgArr, setBgArr] = useState([]);
   const [lastBG, setLastBG] = useState(0);
   const [lastBGtime, setLastBGtime] = useState('');
+  const [addBGlevel, setAddBGlevel] = useState(0);
+  const [startBG, setSBGP] = useState(0);
+  const [reason, setReason] = useState('');
+  const [appointments, setAppointments] = useState([]);
 
   //===========================Functions===============
 
-  const dashBoard = async() => {
+  const addBG = () => {
+    var time = new Date(); // Mon Jan 31 2022 23:07:59 GMT+0300 (+03)
+    var timeString = time.toString();
 
+    try {
+      db.transaction(tx => {
+        tx.executeSql(
+          'INSERT INTO BGLevel (UserID, BGlevel, DateTime) VALUES (?,?,?)',
+          [222, addBGlevel, timeString],
+        );
+        console.log('inserted!!');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    dashBoard();
+
+    if (addBGlevel <= 70) {
+      navigation.navigate('hypo');
+    } else if (addBGlevel > startBG) {
+      //startbgcorr =150
+      navigation.navigate('Calc');
+    }else if (reason == '1'){
+      navigation.navigate('Calc');
+    }
+    
+  };
+
+  const dashBoard = async () => {
     await retrieveDash2();
-    
-    var w =0;
-    var a=0;
-    var u=0;
-    
+
+    var w = 0;
+    var a = 0;
+    var u = 0;
 
     for (let i = 0; i < bgArr.length; i++) {
-      if (bgArr[i] >= fromBGHome && bgArr[i] <= toBGHome){
+      if (bgArr[i] >= fromBGHome && bgArr[i] <= toBGHome) {
         w++;
-      } else if (bgArr[i] > toBGHome){
+      } else if (bgArr[i] > toBGHome) {
         a++;
-      }else if (bgArr[i] < fromBGHome){
+      } else if (bgArr[i] < fromBGHome) {
         u++;
       }
     }
 
     var total = w + a + u;
-    var m = (a/total)*100;
-    if (m >= 50){
+    var m = (a / total) * 100;
+    if (m >= 50) {
       setFlag(true);
-    }else{
+    } else {
       setFlag(false);
     }
 
     setWitin(w);
     setAbove(a);
     setUnder(u);
-
   };
   //==================================================
   //===========================RETRIVE================
   const retrieveDash = () => {
-    // insulinPen table
     try {
       db.transaction(tx => {
         tx.executeSql(
@@ -91,8 +122,6 @@ const Home = ({navigation}) => {
                 fromBGHome = rows.item(i).fromBG;
                 toBGHome = rows.item(i).toBG;
 
-              
-
                 return;
               }
             }
@@ -105,7 +134,7 @@ const Home = ({navigation}) => {
   };
 
   //==================================================
-  const retrieveDash2 = async() => {
+  const retrieveDash2 = async () => {
     await retrieveDash();
     var time = new Date(); // Mon Jan 31 2022 23:07:59 GMT+0300 (+03)
     var bgArr1 = [];
@@ -117,19 +146,19 @@ const Home = ({navigation}) => {
           [],
           (tx, results) => {
             var rows = results.rows;
-            var lastString = rows.item(rows.length-1).DateTime;
+            var lastString = rows.item(rows.length - 1).DateTime;
             var d = new Date(lastString);
             var momFormat = moment(d).format('yyyy/MM/DD  hh:mm a');
 
             setLastBGtime(momFormat);
-            setLastBG(rows.item(rows.length-1).BGlevel);
+            setLastBG(rows.item(rows.length - 1).BGlevel);
 
             for (let i = 0; i < rows.length; i++) {
               var timeString = rows.item(i).DateTime;
               var toObj = new Date(timeString);
               var bgHome = rows.item(i).BGlevel;
               var userid = rows.item(i).UserID;
-              console.log((time - toObj) / (1000 * 60 * 60));
+              //console.log((time - toObj) / (1000 * 60 * 60));
 
               if ((time - toObj) / (1000 * 60 * 60) <= 168) {
                 //168 is the last 7 days in hours
@@ -138,10 +167,10 @@ const Home = ({navigation}) => {
               }
             }
             setBgArr(bgArr1);
-            console.log(bgArr1 + '   This is bg array');
-            console.log(
-              fromBGHome + '  ' + toBGHome + '   This is from and to',
-            );
+            //console.log(bgArr1 + '   This is bg array');
+            // console.log(
+            //   fromBGHome + '  ' + toBGHome + '   This is from and to',
+            // );
           },
         );
       });
@@ -150,6 +179,109 @@ const Home = ({navigation}) => {
     }
   };
 
+  //==================================================
+  const ret3 = () => {
+    try {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT UserID, startBG_correct FROM patientprofile',
+          [],
+          (tx, results) => {
+            var rows = results.rows;
+            for (let i = 0; i < rows.length; i++) {
+              var UID = rows.item(i).UserID;
+              if (UID == 222) {
+                //***************************************FIXIXIXIX */
+
+                var start = rows.item(i).startBG_correct;
+                setSBGP(start);
+              }
+            }
+          },
+        );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //==================================================
+  const ret4 = () => { //Appointments
+    var currentD =  new Date();
+    var counter = 0;
+    var aaa = [];
+    try {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT UserID, appointmentDate FROM appointments',
+          [],
+          (tx, results) => {
+            var rows = results.rows;
+            
+            for (let i = 0; i < rows.length; i++) {
+              //console.log(rows.item(i).appointmentDate + 'iiiiiiiiii');
+              // var UID = rows.item(i).UserID;
+              // if (UID == 222) {
+                 console.log(rows.item(i).appointmentDate + 'iiiiiiiiii');
+                 var d = rows.item(i).appointmentDate;
+                 var toObjd = new Date(d);
+                 var momFormatd = moment(toObjd).format('dddd yyyy/MM/DD');
+                 var weeksNum =  (toObjd-currentD)/(1000 * 60 * 60 *24*7);
+                 var weeksNum2= Math.floor(weeksNum);
+                 if((toObjd-currentD)/(1000 * 60 * 60 *24) > 0){
+                   
+                  //aaa.push(momFormatd+'\n');
+                  aaa.push({key: counter, name: momFormatd, weeks: weeksNum2});
+                  counter++;
+                 }
+
+              // }
+            }
+            setAppointments(aaa);
+            //console.log('this is app array: '+ aaa);
+          },
+        );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //===============================Online DB===========================
+const updateFlag = () => {
+
+      var time = new Date(); 
+      var timeString = time.toString();
+            console.log('in DB of check flag');
+            // eslint-disable-next-line quotes
+            var InsertAPIURL = "http://192.168.56.1/isugar/updateEMsgFlag.php";   //API to  signup
+
+            var headers = {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            };
+            var Data = {
+              UserID: onlinUserID,
+              BGlevel: addBGlevel,
+              Date_Time: timeString,
+
+
+
+            };
+
+          // FETCH func ------------------------------------
+          fetch(InsertAPIURL,{
+              method:'POST',
+              headers:headers,
+              body: JSON.stringify(Data),//convert data to JSON
+          })
+          .then((response)=>response.json()) //check response type of API (CHECK OUTPUT OF DATA IS IN JSON)
+          .then((response)=>{
+          })
+          .catch((error)=>{
+              alert('Error Occured' + error);
+          })
+          }
+//===================================================================
   //==================================================
   const widthScreen = Dimensions.get('window').width;
   const heightScreen = Dimensions.get('window').height;
@@ -187,16 +319,13 @@ const Home = ({navigation}) => {
   };
 
   return (
+    //dashBoard(),
 
-      //dashBoard(),
-
-
-    
-    <LinearGradient colors={['#AABED8', '#fff']} style={styles.container}>
+     <View style={styles.container}>
       <View style={{top: 10, alignItems: 'center'}}>
         <Image source={require('./images/logo.png')} style={styles.pic} />
       </View>
-      <ScrollView style={styles.contView}>
+      <ScrollView>
         <Text
           style={{
             color: '#000',
@@ -209,18 +338,24 @@ const Home = ({navigation}) => {
           Home
         </Text>
 
-        <Text style={styles.inpTxt}>Last BG level Read: {lastBG+'\n'}Time: {lastBGtime}</Text>
+         <View style={styles.innerCotainer}>
 
-        <Text style={styles.inpTxt}>Time in target for the last 7 days:</Text>
-                <TouchableOpacity
+        <Text style={styles.textBody}>Last BG level Read: {lastBG + '\n'}Time: {lastBGtime}
+        </Text>
+
+        <Text style={styles.textBody}>Time in target for the last 7 days:</Text>
+        <TouchableOpacity
           style={{
             marginLeft: 350,
           }}
           onPress={dashBoard}>
-          <Image source={require('./images/upd.png')} style={{height: 25, width: 25,}} />
+          <Image
+            source={require('./images/upd.png')}
+            style={{height: 25, width: 25}}
+          />
         </TouchableOpacity>
 
-        {((bgArr.length > 0)&&(within>0||above>0||under>0)) ? (
+        {bgArr.length > 0 && (within > 0 || above > 0 || under > 0) ? (
           <PieChart
             data={data}
             width={widthScreen}
@@ -234,131 +369,270 @@ const Home = ({navigation}) => {
         ) : (
           <ActivityIndicator size="large" />
         )}
-        {flag? <Text style={styles.msg}>contact diabetes center as you are above target 50% of times</Text> :null}
+        {flag ? (
+          <Text style={styles.msg}>
+            Contact diabetes center as you are above target 50% of times
+          </Text>
+        ) : null}
         <Text>{'\n\n'}</Text>
-      
+         </View>
 
+        <View style={styles.innerCotainer}>
+          <Text style={styles.textBody}>Add new BG level read: </Text>
+
+          <TextInput
+            keyboardType="decimal-pad"
+            placeholder="000.00"
+            onChangeText={value => setAddBGlevel(value)}
+            style={styles.inputT}
+          />
+          <Text style={{fontSize: 15, paddingTop: 15}}>mg/dl</Text>
+
+
+          <Text style={styles.textBody}>BG read reason: </Text>
+
+          <Picker
+            itemStyle={{color: 'black'}}
+            selectedValue={reason}
+            onValueChange={value => setReason(value)}
+            mode="dropdown"
+            style={styles.ddown}>
+            <Picker.Item label="Bedtime" value="0"></Picker.Item>
+            <Picker.Item label="pre- meal/snack" value="1"></Picker.Item>
+            <Picker.Item label="other" value="2"></Picker.Item>
+          </Picker>
+
+          <TouchableOpacity style={styles.buttonV} onPress={addBG}>
+              <Text style={{color:'white', fontSize: 10, textAlign: 'center', fontWeight: 'bold'}}>Add</Text>
+          </TouchableOpacity>
+
+          </View>
+            <View style={styles.innerCotainer2}>
+                    <TouchableOpacity
+          style={{
+            marginLeft: 350,
+          }}
+          onPress={ret4}>
+          <Image
+            source={require('./images/upd.png')}
+            style={{height: 25, width: 25}}
+          />
+        </TouchableOpacity>
+
+            <Text style={styles.textBody}> Upcoming Appointments:{'\n'}
+            </Text>
+            <FlatList
+            data={appointments}
+            renderItem={({ item }) => (<TouchableOpacity onPress={()=>{alert('You have an appointment at the diabetes Center after '+item.weeks+' weeks on '+item.name+'. If you are scheduled to have your annual lab work please don’t forget to do them.')}}><Text style={styles.textBody2}>{item.name}</Text></TouchableOpacity>)}
+              />
+
+
+          <TouchableOpacity  style={styles.buttonR} onPress={()=>{navigation.navigate('appointments')}}>
+           
+              <Text style={{color:'white', fontSize: 15, textAlign: 'center', fontWeight: 'bold'}}>Add an appiontment</Text>
+
+
+  
+            
+          </TouchableOpacity>
+            
+
+            </View>
+            
+        
       </ScrollView>
-    </LinearGradient>
+      
+   </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  prefix: {
-    backgroundColor: '#9c4',
-  },
-  text: {
-    color: '#000',
-    fontSize: 30,
-  },
+
   pic: {
     width: 70,
     height: 90,
   },
-  inputT: {
-    //inputs field
 
-    width: 110,
-    fontSize: 16,
-    shadowColor: '#000',
-    height: 50,
+
+//====================newStyle========================
+container: {
+    flex: 1,
+    backgroundColor: '#EEF0F2',
+  },
+//   pic: {
+//     width: height_logo,
+//     height: height_logo,
+//     marginRight: 10,
+// },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center'
+},
+body: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+    marginBottom: 20,
+
+},
+textBody:{
+    
+    fontSize: 20,
+    color: '#05375a', 
     textAlign: 'center',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 0.62,
-
-    elevation: 2,
-  },
-
-  contView: {
-    //Conten's view
-    backgroundColor: '#fff',
-    height: 550,
-    width: Dimensions.get('window').width,
-    //Dimensions.get("window").width} 360
+    fontWeight: 'bold',
+ }, 
+ textBody2:{
+   marginTop:5,
+   padding: 10,
+    
+  fontSize: 15,
+  backgroundColor: '#506c80', 
+  color: 'white',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  borderRadius: 10,
+}, 
+ innerCotainer: {
+  backgroundColor: 'white', margin: 10, alignItems: 'center',  borderRadius: 15, padding: 5, width: 380,
+       flexDirection: 'row',
+    flexWrap: 'wrap',
     alignSelf: 'center',
-    top: 20,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
+              shadowColor: "#000",
+              shadowOffset: {
+              width: 0,
+              height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              elevation: 5,
+},
+innerCotainer2: {
+  backgroundColor: 'white',
+  alignItems: 'center',
+  borderRadius: 15,
+  paddingBottom: 45,
+  marginBottom:100,
+  width: 380,
+      
+    
+    alignSelf: 'center',
+              shadowColor: "#000",
+              shadowOffset: {
+              width: 0,
+              height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              elevation: 5,
+},
+buttonV: {
+  backgroundColor: '#05375a',
+  alignItems: 'center',
+  width: 50,
+  height: 35,
+  justifyContent: 'center',
+  borderRadius: 15,
+  flexDirection: 'row',
+  shadowColor: "#000",
+              shadowOffset: {
+              width: 0,
+              height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              elevation: 5,
+  
+  
+  
+},
+buttonR: {
 
-    elevation: 7,
-  },
+  backgroundColor: '#05375a',
+  alignItems: 'center',
+  width: 150,
+  height: 35,
+  marginTop:20,
+  justifyContent: 'center',
+  borderRadius: 15,
+  flexDirection: 'row',
+  shadowColor: "#000",
+              shadowOffset: {
+              width: 0,
+              height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              elevation: 5,
+  
+  
+},
+  ddown: {
+  //drop down list style
 
-  inpTxt: {
-    //lables
-    paddingLeft: 20,
-    fontSize: 18,
-    color: 'gray',
+
+  shadowColor: '#000',
+  alignSelf: 'center',
+  width: 140,
+
+
+  alignItems: 'center',
+  
+
+},
+ddown2: {
+  //drop down list style
+
+
+  marginTop: 20,
+  marginLeft: 10,
+  shadowColor: '#000',
+
+  width: 100,
+  fontSize: 5,
+
+  shadowOffset: {
+    width: 0,
+    height: 1,
   },
-    msg: {
+  shadowOpacity: 0.33,
+  shadowRadius: 0.62,
+
+  elevation: 7,
+  backgroundColor: '#f5f5f5',
+},
+  ddown3: {
+  //drop down list style
+
+
+  marginTop: 20,
+  marginLeft: 10,
+  shadowColor: '#000',
+
+  width: 130,
+  fontSize: 5,
+
+  shadowOffset: {
+    width: 0,
+    height: 1,
+  },
+  shadowOpacity: 0.33,
+  shadowRadius: 0.62,
+
+  elevation: 7,
+  backgroundColor: '#f5f5f5',
+},
+picker: {
+  color: 'grey'
+},
+ msg: {
     //lables
     paddingLeft: 20,
     paddingTop: 15,
     fontSize: 18,
     color: 'red',
   },
-
-
-  vNext: {
-    // to make items next to each other
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    paddingTop: 10,
-  },
-
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-    padding: 10,
-    width: 300,
-    alignSelf: 'center',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    paddingLeft: 30,
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 0.9,
-
-    elevation: 3,
-  },
-
-  ddown: {
-    //drop down list style
-
-    paddingLeft: 0,
-    paddingTop: 13,
-    shadowColor: '#000',
-
-    height: 40,
-    width: 160,
-
-    alignItems: 'center',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    
-    shadowOpacity: 0.23,
-    shadowRadius: 0.62,
-
-    elevation: 2,
-    backgroundColor: '#f5f5f5',
-  },
+//====================newStyle========================
 });
 
 export default Home;
