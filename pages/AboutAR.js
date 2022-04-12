6c6bd2796a52b1ba472ef473e26f77e4682cb517
raneemfaisal <77919227+raneemfaisal@users.noreply.github.com>
2022-04-12 14:58:07 +0300

    Update AboutAR.js

--- pages/AboutAR.js
+++ pages/AboutAR.js
@@ -2,45 +2,44 @@
 /* eslint-disable react-native/no-inline-styles */
 
 import React from 'react';
-import {StyleSheet, View, Text, Dimensions, TouchableOpacity, Image} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
-import LinearGradient from 'react-native-linear-gradient';
 
 const App = ({navigation}) => {
   return (
     <View style={styles.container}>
-       <LinearGradient colors={['#E7EFFA', '#E7EFFA','#AABED8']} style={styles.container}>
-       <View style={{top: 10, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', padding: 30}}>
-        <Image source={require('../images/logo.png')} style={styles.pic} />
-        <TouchableOpacity onPress={()=>navigation.openDrawer()}>
+    <View style={{top: 10, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', padding: 30}}>
+         <View style={{alignItems: 'center', marginRight: 130, paddingTop: -10, paddingEnd: -20}}>
+         <Text
+          style={{
+            color: '#05375a',
+            fontSize: 18,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            paddingLeft: 120,
+          }}>
+  حول تطبيق السَّكر
+        </Text>
+         </View>
+                 <TouchableOpacity onPress={()=>navigation.openDrawer()}>
          <Entypo name="menu" color="#05375a" size={35} />
          </TouchableOpacity>
       </View>
-        <Text style={styles.title}>حول تطبيق السَّكر </Text>
+
         <View style={styles.footer}>
         <Text style={styles.titleB}>
         تطبيق السَّكر يهدف إلى تقديم المساعدة في الإدارة اليومية لأطفال مرضى السكري من النوع الأول.
         تطبيق السَّكر يساعد في الحسابات اليومية لجرعات الأنسولين
        </Text>
       </View>
-      </LinearGradient>
     </View>
   );
 };
-const {height} = Dimensions.get('screen');
-const height_logo = height * 0.15;
-
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#EEF0F2',
   },
-  pic: {
-
-    width: height_logo,
-    height: height_logo,
-    marginRight: 10,
-},
   titleB: {
     color: '#05375a',
     fontSize: 25,
@@ -58,10 +57,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginVertical: 5,
   },
-      logo: {
-        width: height_logo,
-        height: height_logo,
-      },
       header: {
         flex: 1,
         justifyContent: 'center',
