e45c353f7e3ce51d24bf70e3c81bf7bed82afac4
Dina-net2 <94227506+Dina-net2@users.noreply.github.com>
2022-05-07 06:25:23 +0300

    Update notConsciousAR.js

--- pages/notConsciousAR.js
+++ pages/notConsciousAR.js
@@ -79,7 +79,7 @@ const notConsciousAR = ({navigation}) => {
   //======================================================
 
   return (
-    <LinearGradient colors={['#AABED8', '#fff']} style={styles.container}>
+    <LinearGradient colors={['#f5f5f5', '#f5f5f5']} style={styles.container}>
       <View style={{top: 10, alignItems: 'center'}}>
         <Image source={require('./images/logo.png')} style={styles.pic} />
       </View>
