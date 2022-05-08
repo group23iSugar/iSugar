fef0d438818c614191d79c84ca8af3ba6c927577
raneemfaisal <77919227+raneemfaisal@users.noreply.github.com>
2022-05-08 21:40:32 +0300

    Update AuthStack.js

--- pages/AuthStack.js
+++ pages/AuthStack.js
@@ -12,11 +12,14 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
+import Foundation from 'react-native-vector-icons/Foundation';
 import AnnualTest from './annualTest';
+import checkFirst from './checkFirst';
 import hypo from './hypo';
-
-
-
+import sickDay from './sickDay';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import fasting from './fasting';
+import pumpFailure from './pumpFailure';
 const Drawer = createDrawerNavigator();
 
 const AuthStack = () => {
@@ -32,9 +35,34 @@ const AuthStack = () => {
             <Ionicons name="home-outline" size={22} color={color}/>
            ),
          }}/>
-         <Drawer.Screen name="Annual Test" component={AnnualTest}options={{
+         <Drawer.Screen name=" Annual Test" component={AnnualTest}options={{
            drawerIcon: ({color}) => (
-            <MaterialIcons name="list-alt" size={22} color={color}/>
+            <Foundation name="clipboard-notes" size={22} color={color}/>
+           ),
+         }}/>
+         <Drawer.Screen name="Exercise" component={checkFirst}options={{
+           drawerIcon: ({color}) => (
+            <MaterialCommunityIcons name="weight-lifter" size={22} color={color}/>
+           ),
+         }}/>
+           <Drawer.Screen name="Hypoglycemia" component={hypo} options={{
+           drawerIcon: ({color}) => (
+            <AntDesign name="infocirlceo" size={22} color={color}/>
+           ),
+         }}/>
+           <Drawer.Screen name="Sick Day" component={sickDay} options={{
+           drawerIcon: ({color}) => (
+            <MaterialIcons name="sick" size={22} color={color}/>
+           ),
+         }}/>
+         <Drawer.Screen name="Fasting" component={fasting}options={{
+           drawerIcon: ({color}) => (
+            <MaterialCommunityIcons name="food-off" size={22} color={color}/>
+           ),
+         }}/>
+         <Drawer.Screen name="Pump Failure" component={pumpFailure}options={{
+           drawerIcon: ({color}) => (
+            <MaterialIcons name="not-interested" size={22} color={color}/>
            ),
          }}/>
          <Drawer.Screen name="About iSugar" component={About} options={{
@@ -48,15 +76,7 @@ const AuthStack = () => {
            ),
          }}/>
          <Drawer.Screen name="s" component={settingsAR} options={{
-          
          }}/>
-
-        <Drawer.Screen name="Hypoglycemia" component={hypo} options={{
-           drawerIcon: ({color}) => (
-            <AntDesign name="infocirlceo" size={22} color={color}/>
-           ),
-         }}/>
-
         </Drawer.Navigator>
     );
 };
