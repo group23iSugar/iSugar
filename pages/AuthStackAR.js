2200c2ba19518ccff887e1eb0c33897ce67aad5e
raneemfaisal <77919227+raneemfaisal@users.noreply.github.com>
2022-05-08 21:43:06 +0300

    Update AuthStackAR.js

--- pages/AuthStackAR.js
+++ pages/AuthStackAR.js
@@ -1,19 +1,19 @@
 /* eslint-disable prettier/prettier */
 
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import About from './AboutAR';
 import settingsAR from './settingsAR';
 import CustomDrawerAR from './CustomDrawerAR';
-import logIn from './logIn';
-import logoScreen from './logoScreen';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
 import { tabHomeAR } from '../tabNavAR';
 import annualTestAR from './annualTestAR';
+import sickDayAR from './sickDayAR';
+import checkFirstAR from './checkFirstAR';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import hypoAR from './hypoAR';
 
 
@@ -27,17 +27,34 @@ const AuthStackAR = () => {
         drawerActiveTintColor:'#fff',
         drawerInactiveTintColor: '#05375a',
          drawerLabelStyle: {marginLeft: -25, fontFamily: 'Roboto-Medium', fontSize: 15}}}>
-            <Drawer.Screen name="tabHomeAR" component={tabHomeAR}options={{
+            <Drawer.Screen name="الصفحة الرئيسية" component={tabHomeAR}options={{
            drawerIcon: ({color}) => (
             <Ionicons name="home-outline" size={22} color={color}/>
            ),
          }}/>
-         <Drawer.Screen name="التحليل السنوي" component={annualTestAR}options={{
+         <Drawer.Screen name="الاختبارات السنوية" component={annualTestAR}options={{
            drawerIcon: ({color}) => (
             <MaterialIcons name="list-alt" size={22} color={color}/>
            ),
          }}/>
-         <Drawer.Screen name="عن التطبيق" component={About} options={{
+
+        <Drawer.Screen name="انخفاض سكر الدم" component={hypoAR} options={{
+           drawerIcon: ({color}) => (
+            <AntDesign name="infocirlceo" size={22} color={color}/>
+           ),
+         }}/>
+
+         <Drawer.Screen name="التمارين" component={checkFirstAR}options={{
+           drawerIcon: ({color}) => (
+            <MaterialCommunityIcons name="weight-lifter" size={22} color={color}/>
+           ),
+         }}/>
+           <Drawer.Screen name="يوم المرض" component={sickDayAR} options={{
+           drawerIcon: ({color}) => (
+            <MaterialIcons name="sick" size={22} color={color}/>
+           ),
+         }}/>
+         <Drawer.Screen name="حول التطبيق" component={About} options={{
            drawerIcon: ({color}) => (
             <AntDesign name="infocirlceo" size={22} color={color}/>
            ),
@@ -47,17 +64,9 @@ const AuthStackAR = () => {
             <Feather name="settings" size={22} color={color}/>
            ),
          }}/>
-         
-        <Drawer.Screen name="انخفاض سكر الدم" component={hypoAR} options={{
-           drawerIcon: ({color}) => (
-            <AntDesign name="infocirlceo" size={22} color={color}/>
-           ),
-         }}/>
-
-
 
         </Drawer.Navigator>
-     
+
     );
 };
 
