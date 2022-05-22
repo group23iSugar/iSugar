d04e6ba9eb3e510183d6c740cb53f6e742e75694
raneemfaisal <77919227+raneemfaisal@users.noreply.github.com>
2022-05-23 00:28:19 +0300

    final update

--- pages/fastingProfile.js
+++ pages/fastingProfile.js
@@ -1,11 +1,19 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-shadow */
+/* eslint-disable eqeqeq */
+/* eslint-disable semi */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, 
+import { StyleSheet,
     View,
-    Image, 
+    Image,
     Text,
     ScrollView,
-    TouchableOpacity, 
-    Dimensions } from 'react-native'; 
+    TouchableOpacity,
+    Dimensions } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
@@ -14,11 +22,11 @@ import { set } from 'react-native-reanimated';
 
 
 
+global.firstFlag = false;
 const fastingProfile = ({ navigation }) => {
     useEffect(() => {
         firstTimeCheck();
         }, []);
-        const [firstFlag, setFirst] = useState(false);
     const firstTimeCheck = () => {
         try {
                         db.transaction(tx => {
@@ -28,8 +36,8 @@ const fastingProfile = ({ navigation }) => {
                             (tx, results) => {
                               var rows = results.rows;
                               for (let i = 0; i < rows.length; i++) {
-                                if (rows.item(i).profileEntry == 'false'){
-                                    setFirst(true);
+                                if (rows.item(i).recordTime == 'false'){
+                                    firstFlag = true;
                                     try {
                                         console.log('in try2');
                                         db.transaction( (tx) => {
@@ -42,16 +50,16 @@ const fastingProfile = ({ navigation }) => {
                                              console.log('record updated seuccefully');
                                              console.log(center);
                                                   }
-                                              }    
-                                    ) 
-                                        
-                                    
-                                    }  ) 
+                                              }
+                                    )
+
+
+                                    }  )
                                     } catch (error) {
                                        console.log(error);
                                     }
                                 }
-                                
+
                               }
                             },
                           );
@@ -60,13 +68,11 @@ const fastingProfile = ({ navigation }) => {
                         console.log(error);
                       }
 
-       
-        
+
+
     };
 
-    const alertMessage = () => {
-      alert('This feature is not available yet!');
-    }
+
   return (
 
     <View style={styles.container}>
@@ -79,32 +85,32 @@ const fastingProfile = ({ navigation }) => {
             paddingTop: 20,
             paddingLeft: 15,
           }}>
-          Fasting Profile
+          Fasting
         </Text>
-         <ScrollView> 
+         <ScrollView>
          <View style={styles.body}>
              {firstFlag == false ? (  <View style={styles.innerCotainer}>
           <Text style={styles.textBody}>- Adjustments made here in Ramadan insulin profile page will be saved here ONLY. (Will NOT be saved in your main profile page)</Text>
           <Text style={styles.textBody}>- Insulin information here in Ramadan profile page will be used for Ramadan fasting section of the application ONLY.</Text>
- 
+
           </View>) : (
                 <View style={styles.innerCotainer}>
           <Text style={styles.textBody}>- Your insulin information in this section has been obtained from your Main profile page and initial adjustment of your ICR/meals sliding scales for Ramadan fasting has been made. You might need to do further adjustments later. </Text>
           <Text style={styles.textBody}>- Adjustments made here in Ramadan insulin profile page will be saved here ONLY. (Will NOT be saved in your main profile page)</Text>
           <Text style={styles.textBody}>- Insulin information here in Ramadan profile page will be used for Ramadan fasting section of the application ONLY.</Text>
- 
+
           </View>
              )}
-         
-      
-        
-        <TouchableOpacity onPress={()=>{navigation.navigate('insulinFasting')}}>
+
+
+
+<TouchableOpacity onPress={()=>{navigation.navigate('insulinFasting')}}>
             <LinearGradient style={styles.outer}
             colors={['#a8dadc', '#457b9d']}>
             <View style={styles.inner}>
             <Text style={styles.textBody}>Insulin Information</Text>
             </View>
-            
+
             </LinearGradient>
             </TouchableOpacity>
      {/* //=================// */}
@@ -114,10 +120,10 @@ const fastingProfile = ({ navigation }) => {
             <View style={styles.inner}>
             <Text style={styles.textBody}>ISF Information</Text>
             </View>
-            
+
             </LinearGradient>
             </TouchableOpacity>
-           
+
             {/* //=================// */}
             <TouchableOpacity onPress={()=>{navigation.navigate('icrFasting')}}>
             <LinearGradient style={styles.outer}
@@ -125,20 +131,20 @@ const fastingProfile = ({ navigation }) => {
             <View style={styles.inner}>
             <Text style={styles.textBody}>ICR Information</Text>
             </View>
-            
+
             </LinearGradient>
             </TouchableOpacity>
-
+            {/* //=================// */}
            </View>
         </ScrollView>
-    </View>      
+    </View>
 
-    
+
   );
 };
 
 
-const {height} = Dimensions.get("screen");
+const {height} = Dimensions.get('screen');
 const height_logo = height * 0.28;
 
 export default fastingProfile;
@@ -149,7 +155,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#EEF0F2',
       },
       innerTitle: {
-        fontSize: 15, color: '#05375a', fontWeight:'bold', textAlign: 'center'
+        fontSize: 15, color: '#05375a', fontWeight:'bold', textAlign: 'center',
     },
       pic: {
         width: height_logo,
@@ -158,14 +164,14 @@ const styles = StyleSheet.create({
     },
       header: {
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     body: {
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 50,
         marginBottom: 20,
-    
+
     },
     outer: {
         width: 275,
@@ -176,7 +182,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderRadius: 15,
         flexDirection: 'row',
-        shadowColor: "#000",
+        shadowColor: '#000',
         shadowOffset: {
         width: 0,
         height: 2,
@@ -191,34 +197,34 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
-        backgroundColor: 'white'
-        
+        backgroundColor: 'white',
+
       },
     textHeader:{
        fontSize: 18,
        paddingBottom: 15,
        paddingTop: 15,
-       color: '#05375a', 
+       color: '#05375a',
        textAlign: 'left',
        paddingLeft: 10,
-       paddingRight: 10
+       paddingRight: 10,
     },
     textAlert:{
       fontSize: 17,
-      color: 'red', 
+      color: 'red',
       fontWeight: 'bold',
       textAlign: 'center',
       paddingLeft: 10,
-      paddingRight: 10
+      paddingRight: 10,
     },
     textBody:{
         fontSize: 20,
         marginBottom: 15,
         paddingBottom: 15,
-        color: '#05375a', 
+        color: '#05375a',
         textAlign: 'left',
         fontWeight: 'bold',
-     }, 
+     },
      textInput: {
       width: 50,
       height: 45,
@@ -227,7 +233,7 @@ const styles = StyleSheet.create({
       borderRightWidth: 1,
       borderBottomWidth: 1,
       borderColor: '#CACDD1',
-      color: '#000'
+      color: '#000',
     },
     textInputISF: {
       width: 60,
@@ -237,11 +243,11 @@ const styles = StyleSheet.create({
       borderRightWidth: 1,
       borderBottomWidth: 1,
       borderColor: '#CACDD1',
-      color: '#000'
+      color: '#000',
     },
      innerCotainer: {
       backgroundColor: 'white', margin: 10, alignItems: 'center',  borderRadius: 15, padding: 10, width: 310,
-                  shadowColor: "#000",
+                  shadowColor: '#000',
                   shadowOffset: {
                   width: 0,
                   height: 2,
@@ -252,7 +258,7 @@ const styles = StyleSheet.create({
     },
     innerCotainerISF: {
       backgroundColor: 'white', margin: 10,  borderRadius: 15, padding: 10, width: 360,
-                  shadowColor: "#000",
+                  shadowColor: '#000',
                   shadowOffset: {
                   width: 0,
                   height: 2,
@@ -264,7 +270,7 @@ const styles = StyleSheet.create({
     innerCotainer2: {
       backgroundColor: 'white', margin: 10, alignItems: 'center',  borderRadius: 15, padding: 10, width: 360,
       flexDirection: 'row',
-                  shadowColor: "#000",
+                  shadowColor: '#000',
                   shadowOffset: {
                   width: 0,
                   height: 2,
@@ -276,7 +282,7 @@ const styles = StyleSheet.create({
     buttonV: {
       marginTop: 60,
       alignItems: 'center',
-      
+
     },
     buttonR: {
       marginTop: 20,
@@ -285,7 +291,6 @@ const styles = StyleSheet.create({
       width: 50,
       height: 35,
       justifyContent: 'center',
-      alignItems: 'center',
       borderRadius: 15,
       flexDirection: 'row',
     },
@@ -296,73 +301,22 @@ const styles = StyleSheet.create({
       width: 100,
       height: 35,
       justifyContent: 'center',
-      alignItems: 'center',
       borderRadius: 15,
       flexDirection: 'row',
     },
     innerView: {
         flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10, marginTop: 10,
-    }, 
-    container: {
-      flex: 1,
-      backgroundColor: '#EEF0F2',
-    },
-    header: {
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    body: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 50,
-      marginBottom: 20,
-    
-    },
-    outer: {
-      width: 275,
-      height: 110,
-      marginTop: 15,
-      marginBottom: 20,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 15,
-      flexDirection: 'row',
-      shadowColor: "#000",
-      shadowOffset: {
-    width: 0,
-    height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-    inner: {
-      width: 250,
-      height: 110,
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'row',
-      backgroundColor: 'white'
-      
-    },
-    textBody:{
-      fontSize: 17,
-      color: '#05375a', 
-      textAlign: 'center',
-      fontWeight: 'bold',
-      marginTop: 5,
-      paddingTop: 15
     },
       ddown: {
       //drop down list style
-    
-    
+
+
       marginTop: 20,
       shadowColor: '#000',
       alignSelf: 'center',
       width: 300,
-    
-    
+
+
       alignItems: 'center',
       shadowOffset: {
         width: 0,
@@ -370,49 +324,49 @@ const styles = StyleSheet.create({
       },
       shadowOpacity: 0.33,
       shadowRadius: 0.62,
-    
+
       elevation: 7,
       backgroundColor: '#f5f5f5',
     },
     ddown2: {
       //drop down list style
-    
-    
+
+
       marginTop: 20,
       marginLeft: 10,
       shadowColor: '#000',
-    
+
       width: 100,
       fontSize: 5,
-    
+
       shadowOffset: {
         width: 0,
         height: 1,
       },
       shadowOpacity: 0.33,
       shadowRadius: 0.62,
-    
+
       elevation: 7,
       backgroundColor: '#f5f5f5',
     },
       ddown3: {
       //drop down list style
-    
-    
+
+
       marginTop: 20,
       marginLeft: 10,
       shadowColor: '#000',
-    
+
       width: 130,
       fontSize: 5,
-    
+
       shadowOffset: {
         width: 0,
         height: 1,
       },
       shadowOpacity: 0.33,
       shadowRadius: 0.62,
-    
+
       elevation: 7,
       backgroundColor: '#f5f5f5',
     },
@@ -424,9 +378,7 @@ const styles = StyleSheet.create({
     },
     picker: {
       color: 'grey',
-      width: 300
-    }
- 
+      width: 300,
+    },
+
 });
-
-
